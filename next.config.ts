63226886f9c4c0a5c3802c6b9c8de34877457c36
Piyaphon Wu <wpiyaphon@gmail.com>
2025-09-10 01:21:20 +0700

    feat: update image remote patterns and add new Radix UI dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    remotePatterns: [
+      new URL(
+        "https://good-car-market-ap-southeast-7.s3.ap-southeast-7.amazonaws.com/**",
+      ),
+    ],
     qualities: [50, 75, 100],
   },
 };
