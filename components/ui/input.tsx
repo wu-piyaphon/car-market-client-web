4e07c2ba74fa58e6a045d1f70934f236c9d2b142
Piyaphon Wu <wpiyaphon@gmail.com>
2025-09-28 02:55:31 +0700

    feat: update CommandItem and Input components for improved styling, text size and layout adjustments

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,12 +12,7 @@ function Input({ className, value, label, type, endIcon, ...props }: Props) {
   return (
     <div className="relative">
       {!!value && (
-        <FormLabel
-          className={cn(
-            "absolute top-1.5 left-3 lg:top-2",
-            value ? "text-sm lg:text-lg" : "text-base lg:text-lg",
-          )}
-        >
+        <FormLabel className={cn("absolute top-2 left-3 text-lg lg:top-2")}>
           {label}
         </FormLabel>
       )}
@@ -27,9 +22,9 @@ function Input({ className, value, label, type, endIcon, ...props }: Props) {
         data-slot="input"
         placeholder={label}
         className={cn(
-          "flex h-12 w-full min-w-0 rounded-md border border-input bg-transparent px-3 shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 lg:h-15 dark:bg-input/30",
+          "flex h-14 w-full min-w-0 rounded-md border border-input bg-transparent px-3 text-lg shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 lg:h-15 dark:bg-input/30",
           "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
-          value ? "pt-3 text-sm lg:pt-5 lg:text-lg" : "text-base lg:text-lg",
+          value && "pt-4 text-lg lg:pt-5",
           className,
         )}
         {...props}
