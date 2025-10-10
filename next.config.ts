a6d8e23e59023a763436e99e9b40fd05bcb761e1
Piyaphon Wu <wpiyaphon@gmail.com>
2025-10-10 20:53:29 +0700

    fix: update image remote patterns for asset handling

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL(
-        "https://good-car-market-ap-southeast-7.s3.ap-southeast-7.amazonaws.com/**",
-      ),
+      {
+        protocol: "https",
+        hostname: "car-market-storage-bucket.s3.ap-southeast-7.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
     qualities: [50, 75, 100],
   },
